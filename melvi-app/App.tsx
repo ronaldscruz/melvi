56e5c002d298859f37e56d1332a1c186c10c8f34
Ronald <ronald.scruz16@gmail.com>
2020-01-22 08:44:03 -0300

    Added some initial packages

--- melvi-app/App.tsx
+++ melvi-app/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
-export default function App() {
+const ViewCenteredContent = styled.View`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TitleText = styled.Text`
+  font-weight: bold;
+  font-size: 14;
+`;
+
+const App = () => {
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <ViewCenteredContent>
+      <TitleText>Open up App.tsx to start working on your app!</TitleText>
+    </ViewCenteredContent>
   );
-}
+};
+
+export default App;
