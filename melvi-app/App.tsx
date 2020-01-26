74302c104f0f3fde99f0b836dcb4d5edc91594d8
ronaldscruz <ronald.scruz16@gmail.com>
2020-01-26 13:59:06 -0300

    Retrieving token from Apollo

--- melvi-app/App.tsx
+++ melvi-app/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import Routes from './src/routes';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  link: createHttpLink({
+    uri: 'http://192.168.1.2:4500/graphql',
+  }),
+  cache: new InMemoryCache(),
+});
 
 const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <ApolloProvider client={client}>
+      <Routes />
+    </ApolloProvider>
+  );
 };
 
 export default App;
--- melvi-app/src/pages/SignIn/index.tsx
+++ melvi-app/src/pages/SignIn/index.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useLazyQuery, useQuery } from '@apollo/react-hooks';
+
 import { MIDNIGHT_BLUE } from '../../constants/colors';
 
 import CenteredContentView from '../../components/CenteredContentView';
@@ -6,22 +9,48 @@ import FormInput from '../../components/FormInput';
 import SubmitButton from '../../components/SubmitButton';
 import Logo from '../../components/Logo';
 
+const SIGN_IN = gql`
+  query SignIn($email: String!, $password: String!) {
+    login(email: $email, password: $password)
+  }
+`;
+
 const SignIn: React.FC = () => {
-  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [
+    signIn,
+    { called: requestedSignIn, loading: signInLoading, error: signInError, data: token },
+  ] = useLazyQuery(SIGN_IN);
+
+  if (requestedSignIn && signInLoading) console.log('SIGN IN:', email, password);
+  if (signInError) console.log('ERROR SIGNING IN:', signInError);
+  if (token) console.log(token);
 
   return (
     <CenteredContentView bgColor={MIDNIGHT_BLUE}>
       <Logo size="medium" />
-      <FormInput placeholder="E-mail" iconName="envelope" gapBottom={true} />
-      <FormInput placeholder="Password" iconName="key" password />
+      <FormInput
+        placeholder="E-mail"
+        iconName="envelope"
+        gapBottom={true}
+        onChangeText={(e: string): void => setEmail(e)}
+      />
+      <FormInput
+        placeholder="Password"
+        iconName="key"
+        password
+        onChangeText={(p: string): void => setPassword(p)}
+      />
       <SubmitButton
         title="Sign in"
         iconName="chevron-right"
         gapTop
         fulfill
-        disabled={loading}
-        loading={loading}
-        onPress={(): void => setLoading(!loading)}
+        disabled={requestedSignIn && signInLoading}
+        loading={requestedSignIn && signInLoading}
+        onPress={(): void => signIn({ variables: { email, password } })}
       />
     </CenteredContentView>
   );
